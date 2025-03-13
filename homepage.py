ff97c4fca7be72b82e1a90137452ea491063260c
Ujjwal Kumar Singh <ujjwalsingh0407@gmail.com>
2025-03-13 12:22:31 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,1 +1,14 @@
 
+driver.find_element(By.XPATH,'//*[@id="desktop-header-cnt"]/div[2]/nav/div/div[1]/div/a').click()
+time.sleep(2)
+driver.find_element(By.XPATH,'//*[@id="desktop-header-cnt"]/div[2]/nav/div/div[1]/div/div/div/div/li[1]/ul/li[2]/a').click()
+time.sleep(2)
+driver.find_element(By.XPATH,'//*[@id="31598736"]/a/div[1]/div/div/div/picture/img').click()
+time.sleep(2)
+open new tab
+
+driver.find_element(By.XPATH,'//*[@id="sizeButtonsContainer"]/div[2]/div[2]/div[1]/button').click()
+time.sleep(2)
+driver.find_element(By.XPATH,'//*[@id="mountRoot"]/div/div[1]/main/div[2]/div[2]/div[2]/div[2]/div/div[1]')
+time.sleep(2)
+driver.find_
